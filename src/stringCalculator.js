37389faada85fb64db75ffd27e3966c4e3195fd8
Katrín Guðmundsdóttir <katringud17@ru.is>
2018-10-08 14:53:39 +0000

    Returns the number that is entered

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -2,7 +2,8 @@
 function add (number){
 	if(number == ""){
 		return 0;
-	}		
+	}	
+	return parseInt(number);	
 }
 
 module.exports = add;
--- src/stringCalculator.test.js
+++ src/stringCalculator.test.js
@@ -5,3 +5,6 @@ it("should return zero on an empty string", () => {
 	expect(add("")).toBe(0);
 });
 
+it("should return the number when there is only one number in the string", () => {
+	expect(add("1")).toBe(1);
+});
