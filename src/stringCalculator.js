2f7950516c4d1098662e5161e32ad71dfd2f6305
Katrín Guðmundsdóttir <katringud17@ru.is>
2018-10-08 15:02:10 +0000

    Adds two number when divided by comma

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -3,7 +3,14 @@ function add (number){
 	if(number == ""){
 		return 0;
 	}	
+
+	if(number.includes(",")){
+		var numberArray = number.split(",");
+		return parseInt(numberArray[0]) + parseInt(numberArray[1]);
+	}
+	else{
 	return parseInt(number);	
+	}
 }
 
 module.exports = add;
--- src/stringCalculator.test.js
+++ src/stringCalculator.test.js
@@ -8,3 +8,7 @@ it("should return zero on an empty string", () => {
 it("should return the number when there is only one number in the string", () => {
 	expect(add("1")).toBe(1);
 });
+
+it("should return sum of two given numbers", () => {
+	expect(add("1,2")).toBe(3);
+});
